b6e1e6fd24fb206e75d21de1ef3dd1ad64d00222
Marc Andrei <santosmarcandrei1414@gmail.com>
2024-11-29 22:25:19 +0800

    Add project files.

---
+++ Selenium Activity/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Manage().Window.Maximize();
+
+        driver.Navigate().GoToUrl("https://ph.jobstreet.com/");
+
+        var searchJob = driver.FindElement(By.Name("keywords"));
+        searchJob.SendKeys("Selenium");
+
+        var searchWhere = driver.FindElement(By.Name("where"));
+        searchWhere.SendKeys("Oriental Mindoro");
+
+        searchJob.Submit();
+
+
+        if (driver.Url.Contains("keywords=Selenium") && driver.Url.Contains("where=Oriental+Mindoro"))
+        {
+            Console.WriteLine("Test Passed!");
+        }
+        else
+        {
+            Console.WriteLine("Test Failed!");
+        }
+    }
+}
